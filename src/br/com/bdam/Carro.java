b49c904117011bcb432f05a47acb71bf26912497
Bruno Dambroski <bruno3414.dambroski@gmail.com>
2024-09-22 05:28:26 -0400

    first commit

---
+++ src/br/com/bdam/Carro.java
@@ -0,0 +1,33 @@
+package br.com.bdam;
+
+public class Carro {
+
+	private String modelo;
+	private int ano;
+	private int velocidadeMaxima;
+	
+	public String getModelo() {
+		return modelo;
+	}
+	
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+	
+	public int getAno() {
+		return ano;
+	}
+	
+	public void setAno(int ano) {
+		this.ano = ano;
+	}
+	
+	public int getVelocidameMaxima() {
+		return velocidadeMaxima;
+	}
+	
+	public void setVelocidadeMaxima(int velocidadeMaxima) {
+		this.velocidadeMaxima = velocidadeMaxima;
+	}
+
+}
---
+++ src/br/com/bdam/Classe1.java
@@ -0,0 +1,12 @@
+package br.com.bdam;
+
+public class Classe1 {
+
+	public static void main(String[] args) {
+		
+		Carro carro = new Carro();
+		carro.setAno(2024);
+		System.out.print(carro.getAno());
+	}
+
+}
